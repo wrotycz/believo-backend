7cefbbbd7401c3a816907243a11432c5cc895358
Kamil Węglarz <kweglarz@pl.sii.eu>
2018-04-25 15:28:50 +0200

    Add findAllByScenarioId to ScenarioRepository

--- src/main/java/pl/weglarz/believo/repository/CharacterRepository.java
+++ src/main/java/pl/weglarz/believo/repository/CharacterRepository.java
@@ -1,11 +1,15 @@
 package pl.weglarz.believo.repository;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.security.access.prepost.PreAuthorize;
 import pl.weglarz.believo.model.entities.Character;
 
+import java.util.Set;
+
 @RepositoryRestResource(collectionResourceRel = "characters", path = "characters")
 @PreAuthorize("hasAuthority('CAN_ACCESS_DOMAIN')")
 public interface CharacterRepository extends PagingAndSortingRepository<Character, Long> {
+    Set<Character> findAllByScenarioId(@Param("scenarioId") Long scenarioId);
 }
